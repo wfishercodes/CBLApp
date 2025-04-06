743bdbbe29a90798db8745711485228993078834
wfishercodes <wfcodes@outlook.com>
2025-04-06 15:57:51 -0400

    Pages Set up for app
    MyCollection.java
    Wishlist.java
    Settings.java
    updates to the activities
    button set on the homepage
    code enabled to do the trasition between the homepage and the my collection page

---
+++ app/src/main/java/com/example/cblrevamp/WishList.java
@@ -0,0 +1,13 @@
+package com.example.cblrevamp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WishList extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
